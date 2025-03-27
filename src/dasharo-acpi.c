9794e432dacb8c488e2c2f1bb6a209de31a80845
Michał Kopeć <michal.kopec@3mdeb.com>
2025-03-27 16:34:57 +0100

    Address upstream review
    Signed-off-by: Michał Kopeć <michal.kopec@3mdeb.com>

--- src/dasharo-acpi.c
+++ src/dasharo-acpi.c
@@ -1,12 +1,6 @@
 // SPDX-License-Identifier: GPL-2.0+
 /*
  * Dasharo ACPI Driver
- *
- * Copyright (C) 2025 3mdeb Sp. z o.o.
- *
- * This program is free software; you can redistribute it and/or modify
- * it under the terms of the GNU General Public License version 2 as
- * published by the Free Software Foundation.
  */
 
 #include <linux/acpi.h>
@@ -22,7 +16,6 @@ enum dasharo_feature {
 	DASHARO_FEATURE_TEMPERATURE = 0,
 	DASHARO_FEATURE_FAN_PWM,
 	DASHARO_FEATURE_FAN_TACH,
-	DASHARO_FEATURE_FAN_POINTS,
 	DASHARO_FEATURE_MAX,
 };
 
@@ -42,18 +35,26 @@ enum dasharo_fan {
 	DASHARO_FAN_MAX,
 };
 
-static char *dasharo_temp_group_name[DASHARO_TEMPERATURE_MAX] = {
-	[DASHARO_TEMPERATURE_CPU_PACKAGE] = "CPU Package",
-	[DASHARO_TEMPERATURE_CPU_CORE] = "CPU Core",
-	[DASHARO_TEMPERATURE_GPU] = "GPU",
-	[DASHARO_TEMPERATURE_BOARD] = "Board",
-	[DASHARO_TEMPERATURE_CHASSIS] = "Chassis",
-};
+#define MAX_GROUPS_PER_FEAT 8
 
-static char *dasharo_fan_group_name[DASHARO_FAN_MAX] = {
-	[DASHARO_FAN_CPU] = "CPU",
-	[DASHARO_FAN_GPU] = "GPU",
-	[DASHARO_FAN_CHASSIS] = "Chassis",
+static char *dasharo_group_names[DASHARO_FEATURE_MAX][MAX_GROUPS_PER_FEAT] = {
+	[DASHARO_FEATURE_TEMPERATURE] = {
+		[DASHARO_TEMPERATURE_CPU_PACKAGE] = "CPU Package",
+		[DASHARO_TEMPERATURE_CPU_CORE] = "CPU Core",
+		[DASHARO_TEMPERATURE_GPU] = "GPU",
+		[DASHARO_TEMPERATURE_BOARD] = "Board",
+		[DASHARO_TEMPERATURE_CHASSIS] = "Chassis",
+	},
+	[DASHARO_FEATURE_FAN_PWM] = {
+		[DASHARO_FAN_CPU] = "CPU",
+		[DASHARO_FAN_GPU] = "GPU",
+		[DASHARO_FAN_CHASSIS] = "Chassis",
+	},
+	[DASHARO_FEATURE_FAN_TACH] = {
+		[DASHARO_FAN_CPU] = "CPU",
+		[DASHARO_FAN_GPU] = "GPU",
+		[DASHARO_FAN_CHASSIS] = "Chassis",
+	},
 };
 
 #define MAX_CAP_NAME_LEN 16
@@ -68,22 +69,18 @@ struct dasharo_capability {
 
 struct dasharo_data {
 	struct acpi_device *acpi_dev;
-	int sensors_count;
-	int fan_tachs_count;
-	int fan_pwms_count;
-	struct dasharo_capability sensors[MAX_CAPS_PER_FEAT];
-	struct dasharo_capability fan_tachs[MAX_CAPS_PER_FEAT];
-	struct dasharo_capability fan_pwms[MAX_CAPS_PER_FEAT];
+	int cap_counts[DASHARO_FEATURE_MAX];
+	struct dasharo_capability capabilities[DASHARO_FEATURE_MAX][MAX_CAPS_PER_FEAT];
 	struct device *hwmon;
 };
 
 static int dasharo_get_feature_cap_count(struct dasharo_data *data, int feat, int cap)
 {
-	union acpi_object obj[2];
 	struct acpi_object_list obj_list;
+	unsigned long long count = 0;
+	union acpi_object obj[2];
 	acpi_handle handle;
 	acpi_status status;
-	unsigned long long ret = 0;
 
 	obj[0].type = ACPI_TYPE_INTEGER;
 	obj[0].integer.value = feat;
@@ -93,19 +90,20 @@ static int dasharo_get_feature_cap_count(struct dasharo_data *data, int feat, in
 	obj_list.pointer = &obj[0];
 
 	handle = acpi_device_handle(data->acpi_dev);
-	status = acpi_evaluate_integer(handle, "GFCP", &obj_list, &ret);
-	if (ACPI_SUCCESS(status))
-		return ret;
-	return -ENODEV;
+	status = acpi_evaluate_integer(handle, "GFCP", &obj_list, &count);
+	if (!ACPI_SUCCESS(status))
+		return -ENODEV;
+
+	return count;
 }
 
 static int dasharo_read_value_by_cap_idx(struct dasharo_data *data, char *method, int cap, int index, long *value)
 {
-	union acpi_object obj[2];
 	struct acpi_object_list obj_list;
+	unsigned long long val = 0;
+	union acpi_object obj[2];
 	acpi_handle handle;
 	acpi_status status;
-	unsigned long long ret = 0;
 
 	obj[0].type = ACPI_TYPE_INTEGER;
 	obj[0].integer.value = cap;
@@ -115,12 +113,12 @@ static int dasharo_read_value_by_cap_idx(struct dasharo_data *data, char *method
 	obj_list.pointer = &obj[0];
 
 	handle = acpi_device_handle(data->acpi_dev);
-	status = acpi_evaluate_integer(handle, method, &obj_list, &ret);
-	if (ACPI_SUCCESS(status)) {
-		*value = ret;
-		return ret;
-	}
-	return -ENODEV;
+	status = acpi_evaluate_integer(handle, method, &obj_list, &val);
+	if (!ACPI_SUCCESS(status))
+		return -ENODEV;
+
+	*value = val;
+	return val;
 }
 
 static int dasharo_hwmon_read(struct device *dev, enum hwmon_sensor_types type,
@@ -130,42 +128,48 @@ static int dasharo_hwmon_read(struct device *dev, enum hwmon_sensor_types type,
 	int ret = 0;
 	long value;
 
-	if (type == hwmon_temp) {
+	switch (type) {
+	case hwmon_temp:
 		if (attr == hwmon_temp_input) {
 			ret = dasharo_read_value_by_cap_idx(data,
-							    "GTMP",
-							    data->sensors[channel].cap,
-							    data->sensors[channel].index,
-							    &value);
+				"GTMP",
+				data->capabilities[DASHARO_FEATURE_TEMPERATURE][channel].cap,
+				data->capabilities[DASHARO_FEATURE_TEMPERATURE][channel].index,
+				&value);
 
 			if (ret > 0)
 				*val = value * 1000;
 		}
-	} else if (type == hwmon_fan) {
+		break;
+	case hwmon_fan:
 		if (attr == hwmon_fan_input) {
 			ret = dasharo_read_value_by_cap_idx(data,
-							    "GFTH",
-							    data->fan_tachs[channel].cap,
-							    data->fan_tachs[channel].index,
-							    &value);
+				"GFTH",
+				data->capabilities[DASHARO_FEATURE_FAN_TACH][channel].cap,
+				data->capabilities[DASHARO_FEATURE_FAN_TACH][channel].index,
+				&value);
 
 			if (ret > 0)
 				*val = value;
 		}
-	} else if (type == hwmon_pwm) {
+		break;
+	case hwmon_pwm:
 		if (attr == hwmon_pwm_input) {
 			ret = dasharo_read_value_by_cap_idx(data,
-							    "GFDC",
-							    data->fan_tachs[channel].cap,
-							    data->fan_tachs[channel].index,
-							    &value);
+				"GFDC",
+				data->capabilities[DASHARO_FEATURE_FAN_PWM][channel].cap,
+				data->capabilities[DASHARO_FEATURE_FAN_PWM][channel].index,
+				&value);
 
 			if (ret > 0)
 				*val = value;
 		}
+		break;
+	default:
+		break;
 	}
 
-	return ret;
+	return 0;
 }
 
 static int dasharo_hwmon_read_string(struct device *dev, enum hwmon_sensor_types type,
@@ -173,15 +177,20 @@ static int dasharo_hwmon_read_string(struct device *dev, enum hwmon_sensor_types
 {
 	struct dasharo_data *data = dev_get_drvdata(dev);
 
-	if (channel < data->sensors_count && type == hwmon_temp && attr == hwmon_temp_label) {
-		*str = data->sensors[channel].name;
-		return 0;
-	} else if (channel < data->fan_tachs_count && type == hwmon_fan && attr == hwmon_fan_label) {
-		*str = data->fan_tachs[channel].name;
-		return 0;
+	switch (type) {
+	case hwmon_temp:
+		if (attr == hwmon_temp_label && channel < data->cap_counts[DASHARO_FEATURE_TEMPERATURE])
+		*str = data->capabilities[DASHARO_FEATURE_TEMPERATURE][channel].name;
+		break;
+	case hwmon_fan:
+		if (attr == hwmon_fan_label && channel < data->cap_counts[DASHARO_FEATURE_FAN_TACH])
+		*str = data->capabilities[DASHARO_FEATURE_FAN_TACH][channel].name;
+		break;
+	default:
+		return -EOPNOTSUPP;
 	}
 
-	return -EOPNOTSUPP;
+	return 0;
 }
 
 static umode_t dasharo_hwmon_is_visible(const void *drvdata, enum hwmon_sensor_types type,
@@ -189,14 +198,22 @@ static umode_t dasharo_hwmon_is_visible(const void *drvdata, enum hwmon_sensor_t
 {
 	const struct dasharo_data *data = drvdata;
 
-	if (channel < data->sensors_count && type == hwmon_temp)
-		return 0444;
-
-	if (channel < data->fan_pwms_count && type == hwmon_pwm)
-		return 0444;
-
-	if (channel < data->fan_tachs_count && type == hwmon_fan)
-		return 0444;
+	switch (type) {
+	case hwmon_temp:
+		if (channel < data->cap_counts[DASHARO_FEATURE_TEMPERATURE])
+			return 0444;
+		break;
+	case hwmon_pwm:
+		if (channel < data->cap_counts[DASHARO_FEATURE_FAN_PWM])
+			return 0444;
+		break;
+	case hwmon_fan:
+		if (channel < data->cap_counts[DASHARO_FEATURE_FAN_TACH])
+			return 0444;
+		break;
+	default:
+		break;
+	}
 
 	return 0;
 }
@@ -291,61 +308,22 @@ static const struct hwmon_chip_info dasharo_hwmon_chip_info = {
 	.info = dasharo_hwmon_info,
 };
 
-static void dasharo_fill_sensors(struct dasharo_data *data)
+static void dasharo_fill_feature_caps(struct dasharo_data *data, int feat)
 {
-	int group = 0;
+	int cap_count = 0;
 	int count = 0;
 
-	while (group < DASHARO_TEMPERATURE_MAX) {
-		count = dasharo_get_feature_cap_count(data, DASHARO_FEATURE_TEMPERATURE, group);
+	for (int group = 0; group < MAX_GROUPS_PER_FEAT; ++group) {
+		count = dasharo_get_feature_cap_count(data, feat, group);
 
-		for (int i = 0; i < count && data->sensors_count < MAX_CAPS_PER_FEAT; ++i) {
-			data->sensors[data->sensors_count].cap = group;
-			data->sensors[data->sensors_count].index = i;
-			snprintf(data->sensors[data->sensors_count].name, MAX_CAP_NAME_LEN, "%s %d", dasharo_temp_group_name[group], i);
-			data->sensors_count++;
+		for (unsigned int i = 0; i < count && cap_count < MAX_CAPS_PER_FEAT; ++i) {
+			data->capabilities[feat][cap_count].cap = group;
+			data->capabilities[feat][cap_count].index = i;
+			scnprintf(data->capabilities[feat][cap_count].name, MAX_CAP_NAME_LEN, "%s %d", dasharo_group_names[feat][group], i);
+			cap_count++;
 		}
-
-		group++;
 	}
-}
-
-static void dasharo_fill_fan_tachs(struct dasharo_data *data)
-{
-	int group = 0;
-	int count = 0;
-
-	while (group < DASHARO_FAN_MAX) {
-		count = dasharo_get_feature_cap_count(data, DASHARO_FEATURE_FAN_TACH, group);
-
-		for (int i = 0; i < count && data->fan_tachs_count < MAX_CAPS_PER_FEAT; ++i) {
-			data->fan_tachs[data->fan_tachs_count].cap = group;
-			data->fan_tachs[data->fan_tachs_count].index = i;
-			snprintf(data->fan_tachs[data->fan_tachs_count].name, MAX_CAP_NAME_LEN, "%s %d", dasharo_fan_group_name[group], i);
-			data->fan_tachs_count++;
-		}
-
-		group++;
-	}
-}
-
-static void dasharo_fill_fan_pwms(struct dasharo_data *data)
-{
-	int group = 0;
-	int count = 0;
-
-	while (group < DASHARO_FAN_MAX) {
-		count = dasharo_get_feature_cap_count(data, DASHARO_FEATURE_FAN_PWM, group);
-
-		for (int i = 0; i < count && data->fan_pwms_count < MAX_CAPS_PER_FEAT; ++i) {
-			data->fan_pwms[data->fan_pwms_count].cap = group;
-			data->fan_pwms[data->fan_pwms_count].index = i;
-			snprintf(data->fan_pwms[data->fan_pwms_count].name, MAX_CAP_NAME_LEN, "%s %d", dasharo_fan_group_name[group], i);
-			data->fan_pwms_count++;
-		}
-
-		group++;
-	}
+	data->cap_counts[feat] = cap_count;
 }
 
 static int dasharo_add(struct acpi_device *acpi_dev)
@@ -358,11 +336,9 @@ static int dasharo_add(struct acpi_device *acpi_dev)
 	acpi_dev->driver_data = data;
 	data->acpi_dev = acpi_dev;
 
-	pr_info("Dasharo driver loading\n");
-
-	dasharo_fill_sensors(data);
-	dasharo_fill_fan_tachs(data);
-	dasharo_fill_fan_pwms(data);
+	for (unsigned int i = 0; i < DASHARO_FEATURE_MAX; ++i) {
+		dasharo_fill_feature_caps(data, i);
+	}
 
 	data->hwmon = devm_hwmon_device_register_with_info(&acpi_dev->dev,
 		"dasharo_acpi", data, &dasharo_hwmon_chip_info, NULL);
@@ -379,7 +355,7 @@ static void dasharo_remove(struct acpi_device *acpi_dev)
 
 static const struct acpi_device_id device_ids[] = {
 	{"DSHR0001", 0},
-	{"", 0},
+	{}
 };
 MODULE_DEVICE_TABLE(acpi, device_ids);
 
